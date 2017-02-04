185413517ef440ac35b662e5d1f8f522d1b6220e
thanhtuanvp88 <thanhtuanvp88@gmail.com>
2017-02-04 14:13:32 +0700

    Infrastructure

---
+++ TeduShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private TeduShopDbContext dbContext;
+        private readonly IDbFactory dbFactory;
+
+        public UnitOfWork(IDbFactory factory)
+        {
+            this.dbFactory = factory;
+        }
+
+        public TeduShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
